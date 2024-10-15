ffcccb974a47fed1883b3e4c625072de6a85ec23
Aaditya Patil <aadityapatil1910@gmail.com>
2024-10-15 21:48:04 +0530

    Refined chat window

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -1,50 +1,64 @@
-import React, { useState,useEffect } from 'react'
-import "./Chatbot.scss"
-import attach from '../assets/attach.png'
+import React, { useState, useEffect } from 'react';
+import "./Chatbot.scss";
+import attach from '../assets/attach.png';
+import usericon from '../assets/user.png';
+import uparrow from "../assets/uparrow.png";
+import boticon from "../assets/bot_circular.jpg";
 
 function Chatbot() {
   const [messages, setMessages] = useState([]);
+
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.key === 'Enter') {
-        console.log("Pressed");
-        handleSendMessage();
-      }
+        handleSendMessage('chatbot-answer');
+      } 
     };
-  
+
     document.addEventListener('keydown', handleKeyDown);
-  
+
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
-  const handleSendMessage = () => {
-    let input = document.getElementById('query-input')
-    if(input.value == '') return
-    console.log("Called");
-    let newMsg = input.value
-    input.value = ''
-    setMessages((prev) => ([...prev, newMsg]))
-  }
+
+  const handleSendMessage = (msgType) => {
+    let input = document.getElementById('query-input');
+    if (input.value === '') return;
+
+    let msgContent = input.value;
+    input.value = '';
+
+    setMessages((prev) => [
+      ...prev,
+      { type: msgType, content: msgContent }
+    ]);
+  };
+
   return (
     <div id='chatbot-grid'>
-        <div id='chatbot-window' className='radius-small'>
-          <div id="chat">
-              {messages.map((msg, index) => (
-                <p key={index}>{msg}</p>
-              ))}
-          </div>
-          <div id="query" className='radius-large'>
-            <button>
-              <img src={attach} />
-            </button>
-            <input id='query-input' type="text" name="" placeholder='Message' />
-          </div>
+      <div id='chatbot-window' className='radius-small'>
+        <div id="chat">
+          {messages.map((msg, index) => (
+            <div key={index} className={`chat-message ${msg.type === 'user-query' && 'reverse'}`}>
+              {msg.type === 'chatbot-answer' && 
+                <img src={boticon} alt="" />
+              }
+              <p className={`${msg.type}`}>{msg.content}</p>
+            </div>
+          ))}
         </div>
-        <div id='user-bar' className='radius-large'></div>
-        <div id='upload' className='radius-small'></div>
+        <div id="query" className='radius-large'>
+          <input id='query-input' type="text" placeholder='Message' />
+          <button>
+            <img src={uparrow} onClick={() => handleSendMessage('user-query')} />
+          </button>
+        </div>
+      </div>
+      <div id='user-bar' className='radius-large'></div>
+      <div id='upload' className='radius-small'></div>
     </div>
-  )
+  );
 }
 
-export default Chatbot
+export default Chatbot;
